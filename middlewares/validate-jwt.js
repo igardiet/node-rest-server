a87139e9fcd1af86901e30da2e32bf2cda69e4b7
Iñaki García <igardiet@gmail.com>
2023-06-23 11:15:34 +0200

    feat: validateJWT middleware

---
+++ middlewares/validate-jwt.js
@@ -0,0 +1,38 @@
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const validateJWT = async (req = request, res = response, next) => {
+  const token = req.header('x-token');
+  if (!token) {
+    return res.status(401).json({
+      msg: 'There is no token in the request',
+    });
+  }
+
+  try {
+    const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+    const user = await User.findById(uid);
+
+    if (!user) {
+      return res.status(401).json({
+        msg: 'Token not valid - user non-existent in DB',
+      });
+    }
+
+    if (!user.status) {
+      return res.status(401).json({
+        msg: "Token not valid - user's status: false",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      msg: 'Token not valid',
+    });
+  }
+};
+
+module.exports = { validateJWT };
