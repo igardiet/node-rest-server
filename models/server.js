a5c56913849f42067189a82d02325908ed5189ac
Iñaki García <igardiet@gmail.com>
2023-06-30 23:28:08 +0200

    search and uploads paths on constructor; Error handling middleware and file upload middlewares; search and uploads routes; graceful shutdown

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
+const gracefulFs = require('graceful-fs');
 const { dbConnection } = require('../database/config');
 
 class Server {
@@ -8,8 +10,10 @@ class Server {
     this.port = process.env.PORT || 3000;
     this.paths = {
       auth: '/api/auth',
+      search: '/api/search',
       categories: '/api/categories',
       products: '/api/products',
+      uploads: '/api/uploads',
       users: '/api/users',
     };
     this.connectDB(); // Connection to database
@@ -23,20 +27,44 @@ class Server {
 
   middlewares() {
     this.app.use(cors()); // cors
-    this.app.use(express.json()); // Body parse
+    this.app.use(express.json()); // Body read and parse
     this.app.use(express.static('public')); // Public directory
+    // Error handling middleware
+    this.app.use((err, req, res, next) => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+    // File upload
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: '/tmp/',
+        createParentPath: true
+      })
+    );
   }
 
   routes() {
     this.app.use(this.paths.auth, require('../routes/auth'));
+    this.app.use(this.paths.search, require('../routes/search'));
     this.app.use(this.paths.categories, require('../routes/categories'));
     this.app.use(this.paths.products, require('../routes/products'));
+    this.app.use(this.paths.uploads, require('../routes/uploads'));
     this.app.use(this.paths.users, require('../routes/users'));
   }
 
   listen() {
-    this.app.listen(this.port || 3000, () => {
+    const server = this.app.listen(this.port || 3000, () => {
       console.log(`Server running in port: ${this.port}`);
+    });
+
+    // Graceful shutdown
+    process.on('SIGINT', () => {
+      console.log('Shutting down server...');
+      server.close(() => {
+        gracefulFs.close(); // Close graceful-fs to ensure file operations are completed
+        process.exit(0);
+      });
     });
   }
 }
